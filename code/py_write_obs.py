37fa47b5084fe9d5541968e9e49244cff2eb3f09
Ethan Buchman <ebuchman@uoguelph.ca>
2013-05-23 02:50:27 -0400

    dir cleaning

--- code/py_write_obs.py
+++
@@ -1,9 +0,0 @@
-#from write_observations import write_observations as write_py
-from Cwrite_observations import write_observations as write_c
-
-import time
-
-t0 = time.time()
-write_c()
-t1 = time.time()
-print t1-t0
